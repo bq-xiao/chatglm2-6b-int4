a036c4f58c920ba6ac77bab3630d78e7fdafc6a6
肖保强 <xiaobq@shuhaisc.com>
2026-03-12 16:35:20 +0800

    修改infer脚本加载模型配置json文件

--- infer-glm2.py
+++ infer-glm2.py
@@ -1,7 +1,7 @@
 from model.configuration_chatglm import ChatGLMConfig
 from model.modeling_chatglm import ChatGLMForConditionalGeneration
 
-config = ChatGLMConfig.from_json_file('config.json')
+config = ChatGLMConfig.from_json_file('model/config.json')
 print(f"model config: {config}")
 model = ChatGLMForConditionalGeneration(config, empty_init=False, device='cuda').half()
 model.init_weights()
